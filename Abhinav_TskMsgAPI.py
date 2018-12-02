c38ea3b86d2c38120341f4e2b309d0680cf3b813
abhinavroy02 <abhinavroy02@gmail.com>
2018-12-02 14:27:49 +0530

    Update Abhinav_TskMsgAPI.py

--- Abhinav_TskMsgAPI.py
+++ Abhinav_TskMsgAPI.py
@@ -11,35 +11,35 @@ url = 'https://api.plivo.com/v1/Account/'
 
 get_response1 = requests.get(url + auth_id + '/Number', auth=HTTPBasicAuth(auth_id, auth_token))
 jsonObj = get_response1.json()
-src = jsonObj["objects"][0]["number"]                                                # Take the first number from the response list as source
-dest = jsonObj["objects"][1]["number"]                                               # Take the second number from the response list as destination
+src = jsonObj["objects"][0]["number"]                                # Take the first number from the response list as source
+dest = jsonObj["objects"][1]["number"]                               # Take the second number from the response list as destination
 print " The source phone number is " + src + "\n Destination Phone number is " + dest
 
 """ Get the cash credits from the account before sending a message using Account API """
 
 get_response2 = requests.get(url + auth_id + '/', auth=HTTPBasicAuth(auth_id, auth_token))
-cash_credits_start = float(get_response2.json()["cash_credits"])                     # Convert the unicode response in float and store
+cash_credits_start = float(get_response2.json()["cash_credits"])     # Convert the unicode response in float and store
 print "\n Cash credits beofre sending the sms is " + str(cash_credits_start)
 
 """ Send sms from souce to destination using Message API """
 
 txt = "hello sir how are you"
 post_response1 = requests.post(url + auth_id + '/Message/', auth=HTTPBasicAuth(auth_id, auth_token), json = {"src" : src, "dst" : dest, "text" : txt})
-uuid = post_response1.json()["message_uuid"]                                         # Get the message uuid  
-uuid = '\''.join(uuid)                                                               # Convert the unicode uuid to string
+uuid = post_response1.json()["message_uuid"]                         # Get the message uuid  
+uuid = '\''.join(uuid)                                               # Convert the unicode uuid to string
 
 
 """ Get message details using Message API """
 
 get_response3 = requests.get(url + auth_id + '/Message/' + uuid + '/' , auth=HTTPBasicAuth(auth_id, auth_token))
-amt_charged = float(get_response3.json()["total_amount"])                            # Get the amount charged for sms and convert to float
+amt_charged = float(get_response3.json()["total_amount"])            # Get the amount charged for sms and convert to float
 print "\n Amount charged for sending the sms is " + str(amt_charged)
 
 """ Get the pricing details for sending a sms in US using Pricing API """
 
 params = {'country_iso' : 'US'}
 get_response4 = requests.get(url + auth_id + '/Pricing/' , params , auth=HTTPBasicAuth(auth_id, auth_token))
-outbound_rate = float(get_response4.json()["message"]["outbound"]["rate"])           # Get the outbound sms rate and convert to float
+outbound_rate = float(get_response4.json()["message"]["outbound"]["rate"])  # Get the outbound sms rate and convert to float
 print "\n Outbound rate for sms is " + str(outbound_rate)
 
 """ Compare the amount charged and outbound rate """
@@ -52,7 +52,7 @@ else :
 """ Get the cash credits from the account after sending a message using Account API """
 
 get_response5 = requests.get(url + auth_id + '/', auth=HTTPBasicAuth(auth_id, auth_token))
-cash_credits_end = float(get_response5.json()["cash_credits"])                        # Convert the unicode response in float and store
+cash_credits_end = float(get_response5.json()["cash_credits"])         # Convert the unicode response in float and store
 print "\n Cash credits after sending the sms is " + str(cash_credits_end)
 
 """ Compare the cash credits before and after sending the sms """
